5c5b672eb96858eb423a16042eebb95737fd5077
Gianni Salinetti <gbsalinetti@extraordy.com>
2017-05-21 19:26:12 +0200

    Updated stop and remove cycles

--- wordpress_lab.sh
+++ wordpress_lab.sh
@@ -7,9 +7,22 @@ cnt_db_name=wpmysql
 db_root_pw=scisciola
 
 # Stop wp container
-docker stop ${cnt_wp_name} && docker rm ${cnt_wp_name}
+if (docker ps | grep ${cnt_wp_name} > /dev/null); then
+  echo "Stopping and removing container ${cnt_wp_name}..."
+  docker stop ${cnt_wp_name} > /dev/null && docker rm ${cnt_wp_name} > /dev/null
+elif (docker ps -a | grep ${cnt_wp_name} > /dev/null); then
+  echo "Removing stopped container ${cnt_wp_name}..."
+  docker rm ${cnt_wp_name} > /dev/null
+fi
+
 # Stop db container
-docker stop ${cnt_db_name} && docker rm ${cnt_db_name}
+if (docker ps | grep ${cnt_db_name} > /dev/null); then
+  echo "Stopping and removing container ${cnt_db_name}..."
+  docker stop ${cnt_db_name} > /dev/null && docker rm ${cnt_db_name} > /dev/null
+elif (docker ps -a | grep ${cnt_db_name} > /dev/null); then
+  echo "Removing stopped container ${cnt_db_name}..."
+  docker rm ${cnt_db_name} > /dev/null
+fi
 
 # Run mysql container
 docker run --name ${cnt_db_name} -e MYSQL_ROOT_PASSWORD=${db_root_pw} -d ${img_db_name}:latest
